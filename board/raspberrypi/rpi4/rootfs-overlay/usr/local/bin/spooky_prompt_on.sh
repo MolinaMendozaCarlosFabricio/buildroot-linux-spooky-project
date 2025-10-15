8a1414c5f7d7e4d03e29b35de8e24f7fd54061ab
kev <moralesperezkevin7@gmail.com>
2025-10-15 09:10:41 -0600

    add: Modify spooky_prompt_on.sh

--- board/raspberrypi/rpi4/rootfs-overlay/usr/local/bin/spooky_prompt_on.sh
+++ board/raspberrypi/rpi4/rootfs-overlay/usr/local/bin/spooky_prompt_on.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
-SPOOKY_PS1='\\[\\e[1;31m\\]\\u (666)@\\h \\[\e[0m\\]:\\w\\$ '
-
-echo "export PROMPT_COMMAND='PS1=\"$SPOOKY_PS1\"'" > /etc/profile.d/spooky_prompt.sh
+#!/bin/sh
+export PS1='\[\033[1;31m\]\u (666)@\h\[\033[0m\]:\w\$ '
+echo "export PS1='\[\033[1;31m\]\u (666)@\h\[\033[0m\]:\w\$ '" > /etc/profile.d/spooky_prompt.sh
